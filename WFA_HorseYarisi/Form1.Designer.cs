57b768a0fcc88a688d8a37654dac9c7b74e0b661
muratvuranok <mrtvrnk@gmail.com>
2020-02-19 14:06:03 +0300

    Add project files.

---
+++ WFA_HorseYarisi/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WFA_HorseYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pcb1 = new System.Windows.Forms.PictureBox();
+            this.pcb2 = new System.Windows.Forms.PictureBox();
+            this.pcb3 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBilgilendirme = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-3, 509);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1640, 10);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-3, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1640, 10);
+            this.label2.TabIndex = 0;
+            // 
+            // pcb1
+            // 
+            this.pcb1.Image = global::WFA_HorseYarisi.Properties.Resources.horse1;
+            this.pcb1.Location = new System.Drawing.Point(0, 104);
+            this.pcb1.Name = "pcb1";
+            this.pcb1.Size = new System.Drawing.Size(325, 193);
+            this.pcb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb1.TabIndex = 1;
+            this.pcb1.TabStop = false;
+            // 
+            // pcb2
+            // 
+            this.pcb2.Image = global::WFA_HorseYarisi.Properties.Resources.horse3;
+            this.pcb2.Location = new System.Drawing.Point(0, 313);
+            this.pcb2.Name = "pcb2";
+            this.pcb2.Size = new System.Drawing.Size(325, 193);
+            this.pcb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb2.TabIndex = 1;
+            this.pcb2.TabStop = false;
+            // 
+            // pcb3
+            // 
+            this.pcb3.Image = global::WFA_HorseYarisi.Properties.Resources.horse5;
+            this.pcb3.Location = new System.Drawing.Point(0, 522);
+            this.pcb3.Name = "pcb3";
+            this.pcb3.Size = new System.Drawing.Size(325, 193);
+            this.pcb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb3.TabIndex = 1;
+            this.pcb3.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(1498, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(278, 62);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Yarışı Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(592, 12);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(616, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(1627, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 609);
+            this.label3.TabIndex = 4;
+            // 
+            // lblBilgilendirme
+            // 
+            this.lblBilgilendirme.AutoSize = true;
+            this.lblBilgilendirme.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBilgilendirme.ForeColor = System.Drawing.Color.White;
+            this.lblBilgilendirme.Location = new System.Drawing.Point(12, 23);
+            this.lblBilgilendirme.Name = "lblBilgilendirme";
+            this.lblBilgilendirme.Size = new System.Drawing.Size(0, 31);
+            this.lblBilgilendirme.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1654, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 151);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1654, 331);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 151);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1654, 539);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 151);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseEnter += new System.EventHandler(this.pictureBox3_MouseEnter);
+            // 
+            // btnReset
+            // 
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(1214, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(278, 62);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Sıfırla";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1788, 716);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblBilgilendirme);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pcb1);
+            this.Controls.Add(this.pcb2);
+            this.Controls.Add(this.pcb3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pcb3;
+        private System.Windows.Forms.PictureBox pcb2;
+        private System.Windows.Forms.PictureBox pcb1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBilgilendirme;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ WFA_HorseYarisi/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_HorseYarisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        Random rnd = new Random();
+
+        // Verdiğiniz 2 değer arasında rastgele sayı üretir.
+        // Benzersiz sayı üretme özelliği yoktur. aynı sayıyı peş peşe üretebilir.
+        // Maximum verdiğiniz değerin, herzaman için bir alt değerini üretir. max olarak verilen değer kesinlikle üretilmez.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //pcb1.Top    => get; set;
+            //pcb1.Right  => get;
+            //pcb1.Bottom => get;
+            //pcb1.Left   => get; set;
+            pcb1.Left += rnd.Next(1, 16); // max => 15
+            pcb2.Left += rnd.Next(1, 16); // max => 15
+            pcb3.Left += rnd.Next(1, 16); // max => 15
+
+
+            if (pcb1.Right > pcb2.Right && pcb1.Right > pcb3.Right)
+            {
+                lblBilgilendirme.Text = "Yarışı 1. kulvardaki Şahbatur önde götürüyor.";
+            }
+            else if (pcb2.Right > pcb1.Right && pcb2.Right > pcb3.Right)
+            {
+                lblBilgilendirme.Text = "Yarışı 2. kulvardaki Gülbatur önde götürüyor.";
+            }
+            else if (pcb3.Right > pcb2.Right && pcb3.Right > pcb1.Right)
+            {
+                lblBilgilendirme.Text = "Yarışı 3. kulvardaki Canbatur önde götürüyor.";
+            }
+
+
+
+
+            if (pcb1.Right > label3.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("Yarışı 1. Kulvardaki Şahbatur Kazandı");
+            }
+            else if (pcb2.Right > label3.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("Yarışı 2. Kulvardaki Gülbatur Kazandı");
+            }
+            else if (pcb3.Right > label3.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("Yarışı 3. Kulvardaki Canbatur Kazandı");
+            }
+        }
+
+        #region Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = 10;
+            trackBar1.Maximum = 400;
+            timer1.Interval = trackBar1.Minimum;
+        }
+        #endregion
+
+        #region TrackBar
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+        }
+        #endregion
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pcb1.Left += 10;
+        }
+
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            pcb2.Left += 10;
+        }
+
+        private void pictureBox3_MouseEnter(object sender, EventArgs e)
+        {
+            pcb3.Left += 10;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            pcb1.Left = pcb2.Left = pcb3.Left = 0;
+            lblBilgilendirme.Text = "";
+            trackBar1.Value = trackBar1.Minimum;
+        }
+    }
+}
